2f91a77671f801f95ee18396095c91c9fe260ee0
mrkerri <tolik.kerimov@mail.com>
2018-02-28 21:55:32 +0200

    .

---
+++ src/lesson9/File.java
@@ -0,0 +1,45 @@
+package lesson9;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class File {
+    public static String read(String path) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(path));
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            String currentString;
+            while ((currentString = reader.readLine()) != null) {
+                stringBuilder.append(currentString);
+                stringBuilder.append("\n");
+            }
+            return stringBuilder.toString();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Error");
+
+        } catch (IOException e) {
+            System.out.println("Error 2");
+        }
+
+        finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error finally");
+            }
+        }
+        return " ";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(read("D:\\Java course\\Main_School.tt"));
+    }
+}
---
+++ src/lesson9/File_2.java
@@ -0,0 +1,36 @@
+package lesson9;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class File_2 {
+        public static String read(String path) {
+            try(BufferedReader reader = new BufferedReader(new FileReader(path))) {
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+                String currentString;
+                while ((currentString = reader.readLine()) != null) {
+                    stringBuilder.append(currentString);
+                    stringBuilder.append("\n");
+                }
+                return stringBuilder.toString();
+            }
+            catch(FileNotFoundException e){
+                System.out.println("Error");
+
+            } catch (IOException e) {
+                System.out.println("Error 2");
+            }
+
+            return " ";
+        }
+
+        public static void main(String[] args) {
+            System.out.println(read("D:\\Java course\\Main_School.tt"));
+        }
+    }
+
+
